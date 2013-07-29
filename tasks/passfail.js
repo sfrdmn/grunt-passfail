610cc6b421cf41c9767f1de586e6da0c0258113d
Sean Fridman <fridman@mail.sfsu.edu>
2013-07-28 23:37:16 -0700

    Wrote task, using errorcount and warncount to detect success/fail

--- tasks/passfail.js
+++ tasks/passfail.js
@@ -10,41 +10,63 @@
 
 module.exports = function(grunt) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+  var _ = grunt.util._
 
-  grunt.registerMultiTask('passfail', 'Your task description goes here.', function() {
+  // Set force option when module loaded so it will apply to all tasks
+  // Requires the configuration to have already been initialized?
+  var force = grunt.config(['passfail', 'force']);
+  // Default is true
+  if (typeof force === 'undefined' || force) {
+    grunt.option('force', true)
+  }
+
+  grunt.registerMultiTask('passfail', 'Run functions when your tasks complete or fail.', function() {
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
-      punctuation: '.',
-      separator: ', '
+      force: true,
+      warnerrorFn: false,
+      error: false,
+      warn: false,
+      success: false,
+      ignoreWarn: false,
+      ignoreError: false
     });
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
+    var name = this.name || 'passfail';
+    var warnerrorFn = grunt.config([name, 'warnerror']);
+    var errorFn = grunt.config([name, 'error']);
+    var warnFn = grunt.config([name, 'warn']);
+    var successFn = grunt.config([name, 'success']);
+    var ignoreWarn = grunt.config([name, 'ignoreWarn']);
+    var ignoreError = grunt.config([name, 'ignoreError']);
+    var warncount = grunt.fail.warncount
+    var errorcount = grunt.fail.errorcount
+    var isWarn = warncount > 0
+    var isError = errorcount > 0
+
+    if (warnerrorFn && (isWarn || isError) && _.isFunction(warnerrorFn)) {
+      warnerrorFn.apply(this, [warncount, errorcount]);
+    }
+    if (errorFn && isError && _.isFunction(errorFn)) {
+      errorFn.call(this, errorcount);
+    }
+    if (warnFn && isWarn && _.isFunction(warnFn)) {
+      warnFn.call(this, warncount);
+    }
+    if (successFn && _.isFunction(error)) {
+      // If warning or error, figure out whether it's ignored as success condition
+      if (isWarn || isError) {
+        if ((ignoreWarn && ignoreError) ||
+            (ignoreWarn && !isError) ||
+            (ignoreError && !isWarn)) {
+          successFn.call(this);
         }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
+      // Otherwise it's a success
+      } else {
+        successFn.call(this);
+      }
+    }
 
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
-
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
-    });
   });
 
 };
